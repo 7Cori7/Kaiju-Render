4481924c2421f7d60c6fa89852d23d710b603b67
Corina <corinaidldesign@gmail.com>
2024-09-03 12:32:21 -0400

    Update mailer.js

--- helpers/mailer.js
+++ helpers/mailer.js
@@ -2,6 +2,7 @@ const nodemailer = require("nodemailer");
 
 //*TRANSPORTER PARA MANDAR CORREOS CON NODEMAILER:
 const transporter = nodemailer.createTransport({
+    service: "Gmail",
     host: "smtp.gmail.com",
     port: 465,
     secure: true,
